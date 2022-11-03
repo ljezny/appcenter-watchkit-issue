a02b79e34522f8b6c1cb991ac6137b565de63351
Lukas Jezny <ljezny@gmail.com>
2022-11-03 09:11:29 +0100

    Initial commit

---
+++ AppCenterIssue/AppCenterIssue/AppCenterIssueApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppCenterIssueApp.swift
+//  AppCenterIssue
+//
+//  Created by Lukas Jezny on 03.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct AppCenterIssueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppCenterIssue/AppCenterWatch Watch App/AppCenterWatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppCenterWatchApp.swift
+//  AppCenterWatch Watch App
+//
+//  Created by Lukas Jezny on 03.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct AppCenterWatch_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
